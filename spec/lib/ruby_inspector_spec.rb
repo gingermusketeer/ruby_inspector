88c1626d64ccaa7705f421658d23a615a5b88e9f
Max Brosnahan <maximilianbrosnahan@gmail.com>
2015-02-25 22:54:47 +1100

    Refactor tests

--- spec/lib/ruby_inspector_spec.rb
+++ spec/lib/ruby_inspector_spec.rb
@@ -6,29 +6,23 @@ describe RubyInspector do
   end
 
   describe '.enable' do
-    it 'sends an initialization message to the ruby inspector server' do
-      allow(described_class).to receive(:connect)
-      expect(described_class).to receive(:send_info).with(
-        method: 'RubyInspector.initialize',
-        params: {
-          name: 'test app',
-          type: :ruby,
-          description: 'test app description'
-        }
-      )
-      described_class.enable('test app', 'test app description')
+    let(:socket) { double('socket') }
+
+    before do
+      allow(described_class).to receive(:socket).and_return(socket)
     end
-  end
 
-  describe '.send_info' do
-    let(:fake_socket) { double('fake_socket') }
-    it 'serializes the info and appends a null byte delimiter' do
-      allow(described_class).to receive(:socket).and_return(fake_socket)
-      expect(fake_socket).to receive(:puts).with(
-        %({"name":"test"}\0)
-      )
+    context "when the server is running" do
+      let(:init_message) {
+        %({"method":"RubyInspector.initialize","params":{"name":"test app","type":"ruby","description":"test app desc"}}\0)
+      }
 
-      described_class.send_info(name: 'test')
+      it 'sends an initialization message to the ruby inspector server' do
+        expect(described_class).to receive(:connect)
+
+        expect(socket).to receive(:puts).with(init_message)
+        described_class.enable('test app', 'test app desc')
+      end
     end
   end
 end
