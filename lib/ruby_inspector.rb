05d276a6ee06f8a10364ceb399eff54f120f7005
Max Brosnahan <maximilianbrosnahan@gmail.com>
2015-02-27 19:04:13 +1100

    Handle socket not being able to connect initially

--- lib/ruby_inspector.rb
+++ lib/ruby_inspector.rb
@@ -1,5 +1,4 @@
 require 'ruby_inspector/version'
-
 require 'json'
 require_relative './nsa/net_http_tracker'
 require_relative './ruby_inspector/dev_tools_request_tracker'
@@ -7,30 +6,55 @@ require_relative './ruby_inspector/dev_tools_request_tracker'
 module RubyInspector
   DELIMITER = "\0"
   class << self
-    def enable(app_name, description = '')
-      connect
-      send_init_info(app_name, description)
+    def enable(app_name, description)
+      @app_name = app_name
+      @description = description
+
+      begin
+        send_init_info
+      rescue Errno::ECONNREFUSED => e
+        puts "[RubyInspector] Unable to send initialization info during setup"
+      end
 
       ::Nsa::NetHttpTracker.on_request do |net_http_request_tracker|
         DevToolsRequestTracker.new(net_http_request_tracker)
       end
     end
 
+    def disable
+      @socket = nil
+      @initialized = nil
+    end
+
     def send_info(data)
+      begin
+        send_init_info unless initialized?
+        send_socket_msg(data)
+      rescue Errno::ECONNREFUSED => e
+        puts "[RubyInspector] Unable to send data: #{data}"
+      end
+    end
+
+    private
+
+    attr_accessor :app_name, :description
+
+    def initialized?
+      @initialized
+    end
+
+    def socket
+      @socket ||= TCPSocket.new('localhost', 8124)
+    end
+
+    def send_socket_msg(data)
       socket.puts(
         ::JSON.generate(data) + DELIMITER
       )
     end
 
-    private
-
-    attr_accessor :socket
-    def connect
-      @socket = TCPSocket.new('localhost', 8124)
-    end
-
-    def send_init_info(app_name, description)
-      send_info(
+    def send_init_info
+      send_socket_msg(
         method: 'RubyInspector.initialize',
         params: {
           name: app_name,
@@ -38,6 +62,7 @@ module RubyInspector
           description: description
         }
       )
+      @initialized = true
     end
   end
 end
--- spec/lib/ruby_inspector_spec.rb
+++ spec/lib/ruby_inspector_spec.rb
@@ -7,21 +7,51 @@ describe RubyInspector do
 
   describe '.enable' do
     let(:socket) { double('socket') }
+    let(:init_message) {
+      %({"method":"RubyInspector.initialize","params":{"name":"test app","type":"ruby","description":"test app desc"}}\0)
+    }
 
     before do
-      allow(described_class).to receive(:socket).and_return(socket)
+      described_class.disable
     end
 
     context "when the server is running" do
-      let(:init_message) {
-        %({"method":"RubyInspector.initialize","params":{"name":"test app","type":"ruby","description":"test app desc"}}\0)
-      }
-
       it 'sends an initialization message to the ruby inspector server' do
-        expect(described_class).to receive(:connect)
-
+        allow(TCPSocket).to receive(:new).with('localhost', 8124).and_return(
+          socket
+        )
         expect(socket).to receive(:puts).with(init_message)
         described_class.enable('test app', 'test app desc')
+      end
+    end
+
+    context "when the server is not running" do
+      let(:other_message) { %({"method":"testing"}\0) }
+
+      before do
+        allow(TCPSocket).to receive(:new).with('localhost', 8124).and_raise(
+          Errno::ECONNREFUSED
+        )
+      end
+
+      it "catches the exception" do
+        expect(socket).not_to receive(:puts)
+        expect {
+          described_class.enable('test app', 'test app desc')
+        }.not_to raise_error
+      end
+
+      it "resends the init message when more info is sent" do
+        # fails first time
+        described_class.enable('test app', 'test app desc')
+
+        allow(TCPSocket).to receive(:new).with('localhost', 8124).and_return(
+          socket
+        )
+        expect(socket).to receive(:puts).with(init_message)
+        expect(socket).to receive(:puts).with(other_message)
+
+        described_class.send_info(method: "testing")
       end
     end
   end
